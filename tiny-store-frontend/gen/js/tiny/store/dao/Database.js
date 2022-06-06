8e1b03ff78f9059c30a98e554f6fe5c193356244
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2022-06-06 10:44:41 +0300

    Update database service Javascript stub.

---
+++ tiny-store-frontend/gen/js/tiny/store/dao/Database.js
@@ -0,0 +1,266 @@
+Database = {
+	 createStore: function(store) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/createStore.rmi";
+		var parameters = [store];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 updateStore: function(store) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/updateStore.rmi";
+		var parameters = [store];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 deleteStore: function(storeId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/deleteStore.rmi";
+		var parameters = [storeId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getStore: function(storeId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getStore.rmi";
+		var parameters = [storeId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getStoreByName: function(name) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getStoreByName.rmi";
+		var parameters = [name];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 findStoresByOwner: function(ownerName) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/findStoresByOwner.rmi";
+		var parameters = [ownerName];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 createStoreEntity: function(storeId, entity) {
+		var __callback__ = arguments[2];
+		var __scope__ = arguments[3] || window;
+		var url = "js/tiny/store/dao/Database/createStoreEntity.rmi";
+		var parameters = [storeId, entity];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 updateStoreEntity: function(entity) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/updateStoreEntity.rmi";
+		var parameters = [entity];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 deleteStoreEntity: function(entity) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/deleteStoreEntity.rmi";
+		var parameters = [entity];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getStoreEntity: function(entityId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getStoreEntity.rmi";
+		var parameters = [entityId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getStoreEntities: function(storeId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getStoreEntities.rmi";
+		var parameters = [storeId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getStoreEntityByClassName: function(className) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getStoreEntityByClassName.rmi";
+		var parameters = [className];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 createDataService: function(storeId, service) {
+		var __callback__ = arguments[2];
+		var __scope__ = arguments[3] || window;
+		var url = "js/tiny/store/dao/Database/createDataService.rmi";
+		var parameters = [storeId, service];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 updateDataService: function(service) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/updateDataService.rmi";
+		var parameters = [service];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 deleteDataService: function(service) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/deleteDataService.rmi";
+		var parameters = [service];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getDataService: function(serviceId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getDataService.rmi";
+		var parameters = [serviceId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getStoreServices: function(storeId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getStoreServices.rmi";
+		var parameters = [storeId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 createOperation: function(operation) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/createOperation.rmi";
+		var parameters = [operation];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 createServiceOperation: function(service, operation) {
+		var __callback__ = arguments[2];
+		var __scope__ = arguments[3] || window;
+		var url = "js/tiny/store/dao/Database/createServiceOperation.rmi";
+		var parameters = [service, operation];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 updateServiceOperation: function(operation) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/updateServiceOperation.rmi";
+		var parameters = [operation];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 deleteServiceOperation: function(operation) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/deleteServiceOperation.rmi";
+		var parameters = [operation];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getServiceOperations: function(serviceId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getServiceOperations.rmi";
+		var parameters = [serviceId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getServiceOperation: function(operationId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getServiceOperation.rmi";
+		var parameters = [operationId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 createChangeLog: function(changeLog) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/createChangeLog.rmi";
+		var parameters = [changeLog];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 deleteChangeLog: function(storeId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/deleteChangeLog.rmi";
+		var parameters = [storeId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	 getChangeLog: function(storeId) {
+		var __callback__ = arguments[1];
+		var __scope__ = arguments[2] || window;
+		var url = "js/tiny/store/dao/Database/getChangeLog.rmi";
+		var parameters = [storeId];
+
+		this.fetch(url, parameters, __callback__, __scope__);
+	},
+
+	fetch: function(url, parameters, callback, scope) {
+		var responsePromise = fetch(url, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(parameters)
+		});
+
+		var responseOK = true;
+		var jsonPromise = responsePromise.then(response => { 
+			if(!response.ok) {
+				responseOK = false;
+			}
+			if(response.headers.get("Content-Type")) { 
+				return response.json(); 
+			}
+		});
+
+		jsonPromise.then(json => {
+			if(!responseOK) {
+				if(this.errorHandler) {
+					this.errorHandler(json);
+				}
+				console.error(json);
+				return;
+			}
+			if (callback) {
+				callback.call(scope, json);
+			}
+		});
+	}
+};
